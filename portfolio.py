2ef944d0aa7cc0f2da737cf39a6c58dc5f82d09e
sharini05 <sharini.s123@gmail.com>
2024-11-03 12:18:11 +0530

    message

---
+++ portfolio.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title("Welcome to my portfolio")
+st.header("About Me")
+st.write("""
+     Hello! I am Sharini S, a first year ECE student at KGisl institute of technology.\n
+     I am eager to learn about both hardware and software application.\n
+     I have a passion to work as assistant engineer.
+     """)
+
+st.header("Education")
+st.write("""
+       *KGisl institue of technology*,Bachelor of engineering in Electronics and communication engineering
+       """) 
+
+st.header("Skills")
+st.write("""
+       Programming languages known:Python\n
+       Web development:HTML,CSS\n
+       Others:VS code""")   
+
+st.header("contact") 
+st.write("for any further details contact me through email")
+st.write("Email: sha.123@gmail.com")        
+
